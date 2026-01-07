05cce659ad62e917eb8520fdb827c26fcb7c6287
enarroied <eric.narro.ied@gmail.com>
2026-01-07 22:31:17 +0100

    added match case

--- src/algorithms/uuid_functions.py
+++ src/algorithms/uuid_functions.py
@@ -2,15 +2,16 @@ import uuid_utils as uuid
 
 
 def get_uuid(uuid_type, name=""):
-    if uuid_type == "1":
-        return uuid.uuid1()
-    elif uuid_type == "3":
-        return uuid.uuid3(uuid.NAMESPACE_DNS, name=name)
-    elif uuid_type == "4":
-        return uuid.uuid4()
-    elif uuid_type == "5":
-        return uuid.uuid5(uuid.NAMESPACE_DNS, name=name)
-    elif uuid_type == "6":
-        return uuid.uuid6()
-    elif uuid_type == "7":
-        return uuid.uuid7()
+    match uuid_type:
+        case "1":
+            return uuid.uuid1()
+        case "3":
+            return uuid.uuid3(uuid.NAMESPACE_DNS, name=name)
+        case "4":
+            return uuid.uuid4()
+        case "5":
+            return uuid.uuid5(uuid.NAMESPACE_DNS, name=name)
+        case "6":
+            return uuid.uuid6()
+        case "7":
+            return uuid.uuid7()
